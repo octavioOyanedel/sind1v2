34a6ee83496ae6f7733ebd8c52533226a33c16b1
Octavio Oyanedel <octavio.oyanedel@gmail.com>
2020-03-27 15:40:09 +0000

    finalizada navbar con modificación de media queries botón buscar

--- resources/views/components/nav/nav.blade.php
+++ resources/views/components/nav/nav.blade.php
@@ -35,7 +35,7 @@
 
 		<form class="form-inline my-2 my-lg-0 ml-auto">
 			<input class="form-control form-control-sm" type="search" placeholder="Buscar" aria-label="Buscar">
-			<button class="btn btn-outline-white btn-sm my-2 my-sm-0 ml-3" type="submit">Buscar</button>
+			<button id="botonBuscarNav" class="btn btn-outline-white btn-sm my-2 my-sm-0 ml-3" type="submit">Buscar</button>
 		</form>		
 
 	</div>
